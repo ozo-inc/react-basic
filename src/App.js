82e56356b648a48a3f70d9b7c8588fc85af63a05
ono_tatsuya <web-setting001@ozo-inc.com>
2021-09-19 22:52:49 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+// import Basic2 from "./components/Basic2";
+// import BasicUseEffect from "./components/BasicUseEffect";
+import TimerContainer from "./components/TimerContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {/*<Basic2 name="Hooks" />*/}
+        {/*<BasicUseEffect />*/}
+        <TimerContainer />
+      </header>
+    </div>
+  );
+}
+
+export default App;
