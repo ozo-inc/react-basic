8ff976ea6a9fdaf661d304ecf02730d96684867c
ono_tatsuya <web-setting001@ozo-inc.com>
2021-09-20 16:14:23 +0900

    useEffect基礎

---
+++ src/components/Timer.js
@@ -0,0 +1,24 @@
+import React, {useState, useEffect} from 'react'
+
+const Timer = () => {
+  
+  const [count, setCount] = useState(0)
+  const time = () => {
+    setCount(prevCount => prevCount + 1)
+  }
+  
+  useEffect(() => {
+    const interval = setInterval(time, 1000)
+    return () => {
+      clearInterval(interval)
+      console.log('clear')
+    }
+  }, [])
+  return (
+    <div>
+      {count}
+    </div>
+  )
+}
+
+export default Timer
