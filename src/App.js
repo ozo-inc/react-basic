7e50a65a602dc81681a71b2e799d2447a701185e
ono_tatsuya <web-setting001@ozo-inc.com>
2021-09-20 17:07:48 +0900

    useContext基礎

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import './App.css';
 // import Basic2 from "./components/Basic2";
 // import BasicUseEffect from "./components/BasicUseEffect";
 // import TimerContainer from "./components/TimerContainer";
-import ApiFetch from './components/ApiFetch'
+import ApiFetch from './components/ApiFetch';
+import AppContext from './contexts/AppContext';
+import B from './components/B';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/*<Basic2 name="Hooks" />*/}
-        {/*<BasicUseEffect />*/}
-        {/*<TimerContainer />*/}
-        <ApiFetch />
-      </header>
-    </div>
+    <AppContext.Provider value={'value from App.js'}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {/*<Basic2 name="Hooks" />*/}
+          {/*<BasicUseEffect />*/}
+          {/*<TimerContainer />*/}
+          <ApiFetch />
+          <B />
+        </header>
+      </div>
+    </AppContext.Provider>
   );
 }
 
